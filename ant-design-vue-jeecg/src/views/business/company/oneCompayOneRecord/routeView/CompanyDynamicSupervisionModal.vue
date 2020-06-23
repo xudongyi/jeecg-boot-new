bd680d07b7140efeb9e99147aeb44745fc6c3cc9
1239274842@qq.com <Wty960225!>
2020-06-23 15:18:13 +0800

    附件上传修改

--- ant-design-vue-jeecg/src/views/business/company/oneCompayOneRecord/routeView/CompanyDynamicSupervisionModal.vue
+++ ant-design-vue-jeecg/src/views/business/company/oneCompayOneRecord/routeView/CompanyDynamicSupervisionModal.vue
@@ -199,6 +199,7 @@
         this.deleteFiles.push(file.response.message);
       },
       edit (record) {
+        let _this =this;
         this.form.resetFields();
         this.model = Object.assign({}, record);
         this.visible = true;
@@ -208,17 +209,26 @@
         this.model.createName = this.$store.getters.userInfo.realname;
 
         this.$nextTick(() => {
-          this.form.setFieldsValue(pick(this.model,'status','companyId','reportYear','documentType','documentName','content','createName','createTime','updateName','updateTime'))
+          _this.form.setFieldsValue(pick(this.model,'status','companyId','reportYear','documentType','documentName','content','createName','createTime','updateName','updateTime'))
         });
 
         if(record.id){
           //查询所属文件
-          let _this =this;
+
           queryFiles({id:record.id},this.$data.url.queryFile).then((res)=>{
 
             _this.$nextTick(() => {
 
               _this.$refs.uploadRef.initFileListArr(res.result);
+              let arr = [];
+              for(var a=0;a< _this.$refs.uploadRef.fileList.length;a++){
+                arr.push( _this.$refs.uploadRef.fileList[a].response.message)
+              }
+              if(arr.length>0){
+                _this.fileList  = arr.join(",")
+              }else{
+                _this.fileList = '';
+              }
             });
 
           });
@@ -226,8 +236,18 @@
 
         }else{
           //清空上传列表
-          this.$nextTick(() => {
-            this.$refs.uploadRef.initFileList("");
+          _this.$nextTick(() => {
+            _this.$refs.uploadRef.initFileList("");
+            let arr = [];
+
+            for(var a=0;a< _this.$refs.uploadRef.fileList.length;a++){
+              arr.push( _this.$refs.uploadRef.fileList[a].response.message)
+            }
+            if(arr.length>0){
+              _this.fileList  = arr.join(",")
+            }else{
+              _this.fileList = '';
+            }
           });
         }
 
