07e5e8c08d8b3ba7be93bcc861348469b6ad3291
1239274842@qq.com <Wty960225!>
2020-08-31 09:52:50 +0800

    污染源

---
+++ ant-design-vue-jeecg-psoam/src/views/psoam/component/AreaLinkSelect.vue
@@ -0,0 +1,166 @@
+<template>
+  <a-cascader :options="options" :value="innerValue"  placeholder="请选择" @change="handleChange" :disabled="disabled"/>
+</template>
+
+<script>
+
+  import AreaHandler from "./AreaHandler";
+  import Vue from "vue";
+  import { getAction } from '@/api/manage'
+  import {loadAreaDate} from './areaUtil'
+
+  export default {
+    name: "AreaLinkSelect",
+    props: {
+      value: {
+        type: String,
+        required:false
+      },
+      // 组件的类型，可选值：
+      // select 下拉样式
+      // cascader 级联样式（默认）
+      disabled:{
+        type: Boolean,
+        default: false
+      },
+      type: {
+        type: String,
+        default: 'cascader'
+      },
+      width: {
+        type: String,
+        default: '100%'
+      }
+    },
+    // 定义model属性
+    model: {
+      // prop即 父组件使用 v-model 绑定的属性,这个名称是自定义的
+      prop: "value",
+      // event即 子组件会向父组件触发的事件,父组件的 v-model可以监听到这个事件,并将vlaue赋值给v-model绑定的属性
+      event: "input"
+    },
+    data() {
+      return {
+        innerValue: [],
+        usedListeners: ['change'],
+        enums: {
+          type: ['cascader', 'select']
+        },
+        reloading: false,
+        //loadAreaDate
+        areaData:'',
+        //originalData
+        originalAreas:'',
+        options:[]
+
+      }
+    },
+    computed: {
+      _listeners() {
+        let listeners = { ...this.$listeners }
+        // 去掉已使用的事件，防止冲突
+        this.usedListeners.forEach(key => {
+          delete listeners[key]
+        })
+        return listeners
+      },
+      _type() {
+        if (this.enums.type.includes(this.type)) {
+          return this.type
+        } else {
+          console.error(`JAreaLinkage的type属性只能接收指定的值（${this.enums.type.join('|')}）`)
+          return this.enums.type[0]
+        }
+      },
+    },
+    watch: {
+      value: {
+        immediate: true,
+        handler() {
+          this.loadDataByValue(this.value)
+        }
+      },
+    },
+    created() {
+      this.loadAreaDate();
+      this.loadData();
+      this.initAreaData();
+    },
+    methods: {
+      /** 通过 value 反推 options */
+      loadDataByValue(value) {
+        if(!value || value.length==0){
+          this.innerValue = []
+          this.reloading = true;
+          setTimeout(()=>{
+            this.reloading = false
+          },100)
+        }else{
+          this.initAreaData();
+          let arr = this.areaData.getRealCode(value);
+          this.innerValue = arr
+        }
+      },
+      /** 通过地区code获取子级 */
+      loadData() {
+
+
+        let pcaa = Vue.ls.get('sys_areas');
+        let arr = []
+        const province = pcaa['86']
+        Object.keys(province).map(key=>{
+          let arr1 = []
+          const city = pcaa[key];
+          Object.keys(city).map(key2=>{
+            let arr2 = []
+            const qu = pcaa[key2];
+            Object.keys(qu).map(key3=>{
+              arr2.push({value:key3, label:qu[key3]});
+            })
+            arr1.push({value:key2, label:city[key2], children:arr2});
+          })
+          arr.push({value:key, label:province[key],children:arr1});
+        })
+        this.options = arr
+        console.log("options",arr)
+      },
+      /** 判断是否有子节点 */
+      hasChildren(options) {
+        options.forEach(option => {
+          let data = this.loadDataByCode(option.value)
+          option.isLeaf = data.length === 0
+        })
+      },
+      handleChange(values) {
+        this.innerValue = values
+        let value = values[values.length - 1]
+        this.$emit('change', value)
+      },
+      loadAreaDate:async  function(){
+
+        this.originalAreas =   await  loadAreaDate();
+        //再从数据库中获取
+      },
+      initAreaData(){
+        if(!this.areaData){
+          this.areaData = new AreaHandler();
+        }
+      },
+    },
+    model: { prop: 'value', event: 'change' },
+  }
+</script>
+
+<style lang="less" scoped>
+  .j-area-linkage {
+    height:40px;
+    /deep/ .area-cascader-wrap .area-select {
+      width: 100%;
+    }
+
+    /deep/ .area-select .area-selected-trigger {
+      line-height: 1.15;
+    }
+  }
+
+</style>
---
+++ ant-design-vue-jeecg-psoam/src/views/psoam/dataSearch/VocsHistoryData.vue
@@ -0,0 +1,147 @@
+<!--<template>-->
+<!--  <a-card :bordered="false">-->
+<!--    &lt;!&ndash; 查询区域 &ndash;&gt;-->
+<!--    <div class="table-page-search-wrapper">-->
+<!--      <a-form layout="inline" @keyup.enter.native="searchQuery">-->
+<!--        <a-row :gutter="24">-->
+<!--          <a-col :xl="5" :lg="7" :md="8" :sm="24">-->
+<!--            <a-form-item label="所属区域">-->
+<!--              <area-link-select @change="areaChange" type="cascader" v-model="queryParam.area" show-search style="width: 100%" optionFilterProp="children"/>-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+<!--          <a-col :xl="5" :lg="7" :md="8" :sm="24">-->
+<!--            <a-form-item label="监测点位名称">-->
+<!--              <a-select v-model="queryParam.mn" show-search style="width: 100%" optionFilterProp="children">-->
+<!--&lt;!&ndash;                                <a-select-option :value="companyIds">请选择</a-select-option>&ndash;&gt;-->
+<!--                <a-select-option v-for="item in items" :key="item.value" :value="item.key">-->
+<!--                  {{item.value}}-->
+<!--                </a-select-option>-->
+<!--              </a-select>-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+<!--&lt;!&ndash;          <a-col :xl="9" :lg="11" :md="12" :sm="24">&ndash;&gt;-->
+<!--&lt;!&ndash;            <a-form-item label="发布时间">&ndash;&gt;-->
+<!--&lt;!&ndash;              <j-date :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" placeholder="请选择开始时间" class="query-group-cust" v-model="queryParam.dataTime_begin"></j-date>&ndash;&gt;-->
+<!--&lt;!&ndash;              <span class="query-group-split-cust"></span>&ndash;&gt;-->
+<!--&lt;!&ndash;              <j-date :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" placeholder="请选择结束时间" class="query-group-cust" v-model="queryParam.dataTime_end" ></j-date>&ndash;&gt;-->
+<!--&lt;!&ndash;            </a-form-item>&ndash;&gt;-->
+<!--&lt;!&ndash;          </a-col>&ndash;&gt;-->
+<!--&lt;!&ndash;          <a-col :xl="5" :lg="7" :md="8" :sm="24">&ndash;&gt;-->
+<!--&lt;!&ndash;            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">&ndash;&gt;-->
+<!--&lt;!&ndash;              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>&ndash;&gt;-->
+<!--&lt;!&ndash;              <a-button type="primary" @click="toSearchReset" icon="reload" style="margin-left: 8px">重置</a-button>&ndash;&gt;-->
+<!--&lt;!&ndash;            </span>&ndash;&gt;-->
+<!--&lt;!&ndash;          </a-col>&ndash;&gt;-->
+<!--        </a-row>-->
+<!--      </a-form>-->
+<!--    </div>-->
+<!--    &lt;!&ndash; 查询区域-END &ndash;&gt;-->
+
+<!--    &lt;!&ndash; table区域-begin &ndash;&gt;-->
+<!--&lt;!&ndash;    <div>&ndash;&gt;-->
+
+<!--&lt;!&ndash;      <a-table&ndash;&gt;-->
+<!--&lt;!&ndash;        ref="table"&ndash;&gt;-->
+<!--&lt;!&ndash;        size="middle"&ndash;&gt;-->
+<!--&lt;!&ndash;        bordered&ndash;&gt;-->
+<!--&lt;!&ndash;        rowKey="id"&ndash;&gt;-->
+<!--&lt;!&ndash;        :columns="columns"&ndash;&gt;-->
+<!--&lt;!&ndash;        :dataSource="dataSource"&ndash;&gt;-->
+<!--&lt;!&ndash;        :pagination="ipagination"&ndash;&gt;-->
+<!--&lt;!&ndash;        :loading="loading"&ndash;&gt;-->
+<!--&lt;!&ndash;        :scroll="{ x: 3100 }"&ndash;&gt;-->
+<!--&lt;!&ndash;        class="j-table-force-nowrap"&ndash;&gt;-->
+<!--&lt;!&ndash;        @change="handleTableChange">&ndash;&gt;-->
+
+<!--&lt;!&ndash;        <div slot="filterDropdown" >&ndash;&gt;-->
+<!--&lt;!&ndash;          <a-card title="自定义列">&ndash;&gt;-->
+<!--&lt;!&ndash;            <a-checkbox-group @change="onColSettingsChange" v-model="settingColumns" :defaultValue="settingColumns">&ndash;&gt;-->
+<!--&lt;!&ndash;              <a-row>&ndash;&gt;-->
+<!--&lt;!&ndash;                <template v-for="(item,index) in defColumns">&ndash;&gt;-->
+<!--&lt;!&ndash;                  <template v-if="item.key!='rowIndex'&& item.dataIndex!='action'">&ndash;&gt;-->
+<!--&lt;!&ndash;                    <a-col :span="12"><a-checkbox :value="item.dataIndex">{{ item.title }}</a-checkbox></a-col>&ndash;&gt;-->
+<!--&lt;!&ndash;                  </template>&ndash;&gt;-->
+<!--&lt;!&ndash;                </template>&ndash;&gt;-->
+<!--&lt;!&ndash;              </a-row>&ndash;&gt;-->
+<!--&lt;!&ndash;            </a-checkbox-group>&ndash;&gt;-->
+<!--&lt;!&ndash;          </a-card>&ndash;&gt;-->
+<!--&lt;!&ndash;        </div>&ndash;&gt;-->
+<!--&lt;!&ndash;        <a-icon slot="filterIcon" type='setting' :style="{ fontSize:'16px',color:  '#108ee9',width:'100%' }" />&ndash;&gt;-->
+
+<!--&lt;!&ndash;        <span slot="airLevel" slot-scope="text,record">&ndash;&gt;-->
+<!--&lt;!&ndash;          <a-tag :color="tagColors[record.level]" style="width: 90%">&ndash;&gt;-->
+<!--&lt;!&ndash;            {{ text}}&ndash;&gt;-->
+<!--&lt;!&ndash;          </a-tag>&ndash;&gt;-->
+<!--&lt;!&ndash;         </span>&ndash;&gt;-->
+<!--&lt;!&ndash;      </a-table>&ndash;&gt;-->
+<!--&lt;!&ndash;    </div>&ndash;&gt;-->
+
+<!--  </a-card>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--  import {loadAreaDate} from '../component/areaUtil'-->
+<!--  import AreaHandler from "../component/AreaHandler"-->
+<!--  import {querySiteNameAndMn} from "../../requestAction/request";-->
+<!--  import Vue from 'vue'-->
+<!--  import AreaLinkSelect from '../component/AreaLinkSelect'-->
+
+<!--    export default {-->
+<!--      name: "VocsHistoryData",-->
+<!--      components: {-->
+
+<!--        AreaLinkSelect-->
+<!--      },-->
+<!--      data(){-->
+<!--        return {-->
+<!--          queryParam: {-->
+<!--            companyIds:this.$store.getters.userInfo.companyIds.join(',')-->
+<!--          },-->
+<!--          items:[],-->
+<!--          siteOriginal:[],-->
+<!--          areaHandler:''-->
+<!--        }-->
+<!--      },-->
+<!--      methods:{-->
+<!--        initArea(){-->
+<!--          this.areaHandler = new AreaHandler()-->
+<!--        },-->
+<!--        getAreaByCode(text){-->
+<!--          if(!text)-->
+<!--            return ''-->
+<!--          //初始化-->
+<!--          if(this.areaHandler==='')-->
+<!--          {-->
+<!--            this.initArea()-->
+<!--          }-->
+<!--          let arr = [];-->
+<!--          this.areaHandler.getAreaBycode(text,arr);-->
+<!--          return arr[0]+arr[1]+arr[2]-->
+<!--        },-->
+<!--        areaChange(val){-->
+<!--          let _this = this;-->
+<!--          _this.items=[];-->
+<!--          _this.siteOriginal.forEach(e=>{-->
+<!--            if(e.area === val){-->
+<!--              _this.items.push(e)-->
+<!--            }-->
+<!--          })-->
+<!--        },-->
+<!--      },-->
+<!--      mounted(){-->
+<!--        //this.initColumns();-->
+<!--        let that = this;-->
+<!--        querySiteNameAndMn({companyIds:this.$store.getters.userInfo.companyIds.join(',')}).then((res)=>{-->
+<!--          if(res.success){-->
+<!--            console.log("!!",res.result);-->
+<!--            that.siteOriginal = res.result;-->
+<!--            that.items = res.result;-->
+<!--          }-->
+<!--        })-->
+<!--      }-->
+<!--    }-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
