1b55e875764ea9e3d0c04b6fb35e70c84a541014
1239274842@qq.com <Wty960225!>
2020-09-08 19:54:23 +0800

    wu

---
+++ jeecg-boot/jeecg-boot-module-psoam/src/main/java/org/jeecg/modules/business/service/impl/WarnLogServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.business.service.impl;
+
+import org.jeecg.modules.business.entity.WarnLog;
+import org.jeecg.modules.business.mapper.WarnLogMapper;
+import org.jeecg.modules.business.service.IWarnLogService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: warn_log
+ * @Author: jeecg-boot
+ * @Date:   2020-09-08
+ * @Version: V1.0
+ */
+@Service
+public class WarnLogServiceImpl extends ServiceImpl<WarnLogMapper, WarnLog> implements IWarnLogService {
+
+}
