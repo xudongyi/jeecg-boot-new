25069d127750d3ca020f2d7f2e27e7d94ccb2cb4
yunpeng4456 <1441331786@qq.com>
2020-09-21 09:16:40 +0800

    push by wu tianyi

---
+++ ant-design-vue-jeecg-psoam/src/views/psoam/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <a-card :bordered="false">
+    <div class="home-body">
+      <div class="body-left">
+
+      </div>
+      <div class="body-right body-module">
+
+      </div>
+      <div  class="body-buttom body-module">
+
+      </div>
+    </div>
+  </a-card>
+
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+  .home-body{
+    display: flex;
+  }
+  .body-module{
+    background: #FFFFFF;
+    box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+  }
+  .body-left{
+
+
+  }
+</style>
